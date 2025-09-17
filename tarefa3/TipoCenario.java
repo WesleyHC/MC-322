ac14137d43caec0640fc554e163d31de404c9db9
v256506@dac.unicamp.br <victor.m.yamada@gmail.com>
2025-09-16 23:29:57 -0300

    Definição básica do enum de Cenários

--- tarefa3/TipoCenario.java
+++ tarefa3/TipoCenario.java
@@ -5,21 +5,24 @@ import tarefa1.Hero;
 public enum TipoCenario {
     OLIMPO("In the name of Hades! Olympus, I accept this message") {
         public void Effect(Hero heroi){
+            heroi.setSorte(10);
             System.out.println("Inspirado pelo Olimpo");
         }
     },
     ELÍSIO("Teseus e Astério estão me aguardando"){
         public void Effect(Hero heroi){
+            heroi.receberForca(30);
             System.out.println("Ansioso pra encontrar Patroclus");
         }
     },
-    SALÃO("Sala qualquer aí"){
+    CAMPOS("Sala qualquer aí"){
         public void Effect(Hero heroi){
+            heroi.setSorte(-10);
             System.out.println("Medo do Boss");
         }
     };
 
-    private String descricao;
+    private final String descricao;
 
     TipoCenario(String descricao){
         this.descricao = descricao;
