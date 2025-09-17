65efefeed2ef38b12a1badb746b769763f13e6e1
v256506@dac.unicamp.br <victor.m.yamada@gmail.com>
2025-09-16 23:29:31 -0300

    Testes para Construtor de Fases

--- tarefa3/ConstrutorDeCenarioFixo.java
+++ tarefa3/ConstrutorDeCenarioFixo.java
@@ -1,15 +1,59 @@
 package tarefa3;
 
 import java.util.ArrayList;
+import java.util.Random;
 
+import tarefa1.Chimera;
+import tarefa1.Ciclop;
+import tarefa1.Harpy;
+import tarefa1.Monster;
+import tarefa2.Bow;
+import tarefa2.Club;
 import tarefa2.Level;
+import tarefa2.Spear;
+import tarefa2.Sword;
 
 public class ConstrutorDeCenarioFixo implements GeradorDeFases{
+    TipoCenario[] Cenarios = TipoCenario.values();
 
-    @Override
     public ArrayList<Level> gerarFases(int nFases) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'gerarFases'");
+        ArrayList<Level> levels = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < nFases; i++) {
+            int nivelDolvl = i + 1;
+            TipoCenario ambiente = Cenarios[random.nextInt(Cenarios.length)];
+            ArrayList<Monster> monstrosDolvl = new ArrayList<>();
+            int nMonstros = 1 + nivelDolvl;
+
+            for (int j = 0; j < nMonstros; j++) { //up nos atributos dos monstros cada lvl da fase
+                int vidaBonus = 20*nivelDolvl;
+                int forcaBonus = 4*nivelDolvl;
+                int xpBonus = 25*nivelDolvl;
+                
+                if (j == 0) {
+                    Ciclop ciclope = new Ciclop("Ciclope", 40 + vidaBonus, 15 + forcaBonus, new Club(), 15 + xpBonus);
+                    ciclope.addArmaComum(new Sword());
+                    ciclope.addArmaRara(new Club());
+                    monstrosDolvl.add(ciclope);
+                } else if (j == 1) {
+                    Harpy harpia = new Harpy("Harpia", 30 + vidaBonus, 5 + forcaBonus, new Bow(), 10 + xpBonus, 35);
+                    harpia.addArmaComum(new Bow());
+                    harpia.addArmaRara(new Sword());
+                    monstrosDolvl.add(harpia);
+                } else {
+                    Chimera quimera = new Chimera("Quimera", 45 + vidaBonus, 10 + forcaBonus, new Sword(), 20 + xpBonus + 5, 10);
+                    quimera.addArmaComum(new Spear());
+                    quimera.addArmaRara(new Sword());
+                    monstrosDolvl.add(quimera);
+                }
+            }
+            
+           
+            Level newlvl = new Level(nivelDolvl, ambiente, monstrosDolvl);
+            levels.add(newlvl);
+        }
+
+        return levels;
     }
-    
 }
--- tarefa3/FaseDeCombate.java
+++ tarefa3/FaseDeCombate.java
@@ -8,26 +8,76 @@ import tarefa1.Monster;
 public class FaseDeCombate implements Fase{
     private Hero heroi;
     private TipoCenario cenario;
-    private ArrayList<Monster> monstrosDolvl = new ArrayList<>();
+    private ArrayList<Monster> monstrosDoLvl = new ArrayList<>();
     
     public FaseDeCombate(ArrayList<Monster> monstrosDoLvl, TipoCenario cenario){
         this.cenario = cenario;
+        this.monstrosDoLvl = monstrosDoLvl;
     }
+
     @Override
     public void iniciar(Hero heroi) {
         this.heroi = heroi;
     }
 
     @Override
-    public boolean isDone() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isDone'");
+    public boolean isDone(int kills, ArrayList<Monster> TotalMonstros) {
+        if (kills == TotalMonstros.size()){
+            return true;
+        } else {
+            return false;
+        }
+       // retorna true se todos os monstros foram derrotados
     }
 
     @Override
     public TipoCenario getTipoDeCenario() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTipoDeCenario'");
+        return cenario;
     }
     
-}
+    public void run(FaseDeCombate FaseDeCombate){
+        System.out.println(cenario.getDescricao());
+        int kills = 0;
+        for (Monster monstro : monstrosDoLvl) {
+                System.out.println(heroi.getName() + " se depara com um temível oponente: " + monstro.getName());
+                int turno = 1;
+                while (monstro.isAlive() && heroi.isAlive()) {
+                    System.out.println("\n=================|TURNO " + turno + "|===================");
+                    AcaoDeCombate HAction = heroi.escolherAcao(monstro);
+                    HAction.executar(heroi, monstro);
+                    
+
+                    //Verificação se o Monstro morreu
+                    if (!monstro.isAlive()) {
+                        AcaoDeCombate MAction = monstro.escolherAcao(heroi);
+                    MAction.executar(monstro, heroi);
+                    }
+                    heroi.exibirStatus();
+                    monstro.exibirStatus();
+                    System.out.println("==============|FIM DO TURNO " + turno + "|===============");
+                    turno += 1;
+                }
+
+                //Verificação se o Herói morreu e mensagem final (Game Over)
+                if (!heroi.isAlive()){
+                        System.out.println("\n" + heroi.getName() + " é derrotado(a)!\n\nPara os Deuses não há espaço para a fraqueza.");
+                        System.out.println("O Olimpo vira o rosto, e o nome de " + heroi.getName() + " se perde no esquecimento.");
+                        System.out.println(heroi.getName() + " foi considerado(a) indigno(a)!");
+                        System.out.println("\n================|GAME OVER|===================");
+                        System.exit(1);  
+                }
+
+                //Verificação da morte do Monstro e da Experiência concedido
+                if (!monstro.isAlive()){
+                    kills += 1;
+                    System.out.println("\n" + monstro.getName() + " é derrotado(a)");
+                    Item drop = monstro.droparLoot(heroi);
+                    heroi.equipar(drop);
+                    heroi.ganharExperiencia(monstro.getXpConcedido());
+                }
+            }
+            if (isDone(kills, monstrosDoLvl)){
+                return;
+            }
+        }
+    }
